2eb100f3e3963447c595564a8ff3bf2884d529b1
Viktor Kozhemiaka <9075276@gmail.com>
2024-04-22 16:42:20 +0300

    data

--- study.py
+++ study.py
@@ -215,3 +215,17 @@
 # fruits = re.split(pattern, text)
 
 # print(fruits)
+
+from datetime import datetime
+
+def get_days_from_today(date):
+    try:
+        date = datetime.strptime(date, '%Y-%m-%d')  
+    except ValueError:
+        return 0
+    today = datetime.today()
+    delta = today - date
+    return delta.days
+
+print(get_days_from_today('2024-04-23'))
+
