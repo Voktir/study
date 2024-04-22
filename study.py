763d72f2d542bc5fd2e8c11aa1fb409a452bf815
Viktor Kozhemiaka <9075276@gmail.com>
2024-04-22 21:30:20 +0300

    re

--- study.py
+++ study.py
@@ -248,3 +248,38 @@
 
 # lottery_numbers = get_numbers_ticket(1, 49, 6)
 # print("Ваші лотерейні числа:", lottery_numbers)
+
+# import re
+
+# def normalize_phone(phone_number: str) -> str:
+#     plus_split = []
+#     pat_split = r"\+"
+#     plus_split = re.split(pat_split, phone_number)
+#     #print(plus_split)
+#     try:
+#         plus_split[1] = re.sub(r"\D", "", plus_split[1])
+#         plus_split[1] = plus_split[1] if re.search("38..........", plus_split[1]) else f"38{plus_split[1]}"
+#         result = f"+{plus_split[1]}"
+#     except IndexError:
+#         plus_split[0] = re.sub(r"\D", "", plus_split[0])
+#         plus_split[0] = plus_split[0] if re.search("38..........", plus_split[0]) else f"38{plus_split[0]}"
+#         #print(plus_split[0])
+#         result = f"+{plus_split[0]}"
+#     #print(result)
+   
+#     return   result 
+
+# raw_numbers = [
+#     "067\\t123 3867",
+#     "(095) 234-5678\\n",
+#     "+380 44 123 4567",
+#     "380501234567",
+#     "    +38(050)123-32-34",
+#     "     0503451234",
+#     "(050)8889900",
+#     "38050-111-22-22",
+#     "38050 111 22 11   ",
+# ]
+
+# sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+# print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
