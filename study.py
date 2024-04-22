c00adaf927917b9de80250ce545e21a5c31ce950
Viktor Kozhemiaka <9075276@gmail.com>
2024-04-22 19:24:53 +0300

    random

--- study.py
+++ study.py
@@ -216,16 +216,35 @@
 
 # print(fruits)
 
-from datetime import datetime
+# from datetime import datetime
 
-def get_days_from_today(date):
-    try:
-        date = datetime.strptime(date, '%Y-%m-%d')  
-    except ValueError:
-        return 0
-    today = datetime.today()
-    delta = today - date
-    return delta.days
+# def get_days_from_today(date:str) -> int:
+#     try:
+#         date = datetime.strptime(date, '%Y-%m-%d')  
+#     except ValueError:
+#         return 0
+#     today = datetime.today()
+#     delta = today - date
+#     return delta.days
 
-print(get_days_from_today('2024-04-23'))
+# print(get_days_from_today('2024-04-23'))
 
+# import random
+
+# def get_numbers_ticket(min: int, max: int, quantity: int):
+#     if min<1 or max>1000 or max<(min+quantity):
+#         return []
+#     else:
+#         lst = []
+#         min_while = min
+#         while min_while <= max:
+#             lst.append(min_while)
+#             min_while += 1
+#         dif = max-min
+#         while dif >= quantity:
+#             lst.pop(random.randint(0, dif))
+#             dif -= 1 
+#     return lst
+
+# lottery_numbers = get_numbers_ticket(1, 49, 6)
+# print("Ваші лотерейні числа:", lottery_numbers)
